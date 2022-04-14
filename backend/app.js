c8f42d8b035311fcb741b7d1d81178042686c473
Lynerah <celinedeboeck@gmail>
2022-04-14 12:37:07 +0200

    update, delete, put

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const Sauce = require('./models/Sauce');
+const Sauce = require('./models/Sauce');
 
 const userRoutes = require('./routes/user');
 
@@ -24,20 +24,34 @@ app.use(express.json());
 app.use('/api/auth', userRoutes);
 
 app.post('/api/sauces', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+
+  
+  delete req.body._id;
+  const sauce = new Sauce ({
+    ...req.body
   });
-  // delete req.body._id;
-  // const sauce = new Sauce ({
-  //   ...req.body
-  // });
-  // sauce.save()
-  // .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-  // .catch(error => res.status(400).json({ error }));
+  sauce.save()
+  .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+  .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/sauces/:id', (req, res, next) =>{
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+  .catch(error => res.status(400).json({ error }));
+});
 
+app.get('/api/sauces/:id', (req, res, next) =>{
+  Sauce.findOne({ _id: req.params.id })
+.then(sauce => res.status(200).json(sauce))
+.catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+  Sauce.find()
+  .then(sauces => res.status(200).json(sauces))
+  .catch(error => res.status(400).json({ error }));
+})
 
 
 
--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,17 +1,16 @@
-// const mongoose = require('mongoose');
-// 
-// const sauceSchema = new mongoose.Schema({
-//   userId: { type: String, required: true },
-//   name: { type: String, required: true },
-//   manufacturer: { type: String, required: true },
-//   description: { type: String, required: true },
-//   mainPepper: { type: String, required: true },
-//   imageUrl: { type: String, required: true },
-//   heat: { type: Number, required: true },
-//   likes: { type: Number, required: true },
-//   dislikes: { type: Number, required: true },
-//   usersLiked: {type: Array},
-//   usersDisliked: {type:Array}
-// });
-// 
-// module.exports = mongoose.model('Sauce', sauceSchema);
+const mongoose = require('mongoose');
+
+const sauceSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number},
+  usersLiked: {type: Array},
+  usersDisliked: {type:Array}
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
